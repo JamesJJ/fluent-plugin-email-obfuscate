7361e09a0fe07507d97d52c0b3fcebd256bbaa2b
JamesJJ <jj@fcg.fyi>
2018-05-10 16:50:27 +0800

    v0.0.2

--- lib/fluent/plugin/filter_email_obfuscate.rb
+++ lib/fluent/plugin/filter_email_obfuscate.rb
@@ -20,33 +20,76 @@ module Fluent
     class EmailObfuscateFilter < Fluent::Plugin::Filter
       Fluent::Plugin.register_filter("email_obfuscate", self)
 
+      config_param :mode, :string, default: 'partial_name',
+        desc: <<-DESC
+'full' will replace all characters.
+'partial_name' will replace all characters in the 'domain' half of the address, and a subset of the 'name'.
+'domain_only' will only replace characters in the 'domain' half of the address.
+'.' and '@' are never replaced.
+DESC
+
+      config_param :suffix_whitelist, :array, default: [],
+        desc: <<-DESC
+List of email suffixes not to obfuscate e.g. ['@example.com', '.example.com'] would result in:
+user@example.com => unchanged
+user@subdomain.example.com => unchanged
+user@example.net => obfuscated
+DESC
+
+      def configure(conf)
+        super
+
+        if conf.has_key?('mode')
+          raise ConfigError, "'mode' must be one of: domain_only, full, partial_name" unless
+            ['domain_only', 'full', 'partial_name'].include?(conf.dig('mode'))
+        end
+      end
+
+      def hide_partial(str)
+          case
+          when str.length < 5
+            len = str.length
+          when str.length < 11
+            len = (str.length / 2) + 2
+          else
+            len = (str.length / 3) + 4
+          end
+          str[0, len] + '*' * (str.length - len)
+      end
+
       def obfuscate(str)
-        str.match(/^([^@]+)(@.+)$/){|m| m[1] + m[2].tr('@.a-zA-Z0-9','@.*') } || str
+        str = str.match(/^([^@]+)(@.+)$/) { |m|
+           case @mode
+           when 'domain_only'
+             m[1] + m[2].tr("@.a-zA-Z0-9", "@.*")
+           when 'full'
+             m[1].gsub(/./, '*') + m[2].tr("@.a-zA-Z0-9", "@.*")
+           else
+             hide_partial(m[1]) + m[2].tr("@.a-zA-Z0-9", "@.*")
+           end
+        } if @suffix_whitelist.select{ |a| str.downcase.end_with?(a.downcase)}.empty?
+        str
       end
 
       def deep_process(o)
-
         case o
         when String
           os = o.scan(/<([^<>]+@[^<>]+)>/)
-          o = os.length.zero? ? obfuscate(o) : deep_process(os).join(', ')
-
+          o = os.length.zero? ? obfuscate(o) : deep_process(os).join(", ")
         when Array
           o.map! do |obj|
             deep_process(obj)
           end
-
         when Hash
           o.each_pair do |key, value|
             o[key] = deep_process(value)
           end
-
         end
         o
       end
 
       def filter(tag, time, record)
-         deep_process(record)
+        deep_process(record)
       end
     end
   end
