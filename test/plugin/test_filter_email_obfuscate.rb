176c22fdbd1021c8a91fa9233456b3971828625c
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2018-10-03 17:19:31 +0900

    Add testcases

--- test/plugin/test_filter_email_obfuscate.rb
+++ test/plugin/test_filter_email_obfuscate.rb
@@ -6,13 +6,128 @@ class EmailObfuscateFilterTest < Test::Unit::TestCase
     Fluent::Test.setup
   end
 
-  test "failure" do
-    flunk
+  CONF = %[
+  ]
+
+  def test_configure
+    d = create_driver
+    assert_equal :partial_name, d.instance.mode
+    assert_equal [], d.instance.suffix_whitelist
+  end
+
+  test "invalid mode" do
+    assert_raise(Fluent::ConfigError) do
+      create_driver(CONF + %[mode invalid])
+    end
+  end
+
+  test "filter" do
+    d = create_driver
+    sample_records = {
+      "f1": "myEmail@example.net",
+      "list1": [
+        "user1@example.com",
+        "user2@example.org"
+      ],
+      "a": {
+        "nested": {
+          "field": "name3@example.museum"
+        }
+      },
+      "email_string": "Jane Doe <jane@example.name>, John Smith <john@example.name>"
+    }
+    d.run(default_tag: 'test') do
+      d.feed(sample_records)
+    end
+    expected = [{f1: "myEma**@*******.***",
+                 list1: ["user*@*******.***", "user*@*******.***"],
+                 a: {nested:
+                       {field: "name*@*******.******"}},
+                 email_string: "jane@*******.****, john@*******.****"}]
+    assert_equal expected, d.filtered.map{|e| e.last}
+  end
+
+  test "filter_full" do
+    d = create_driver(CONF + %[mode full])
+    sample_records = {
+      "f1": "myEmail@example.net",
+      "list1": [
+        "user1@example.com",
+        "user2@example.org"
+      ],
+      "a": {
+        "nested": {
+          "field": "name3@example.museum"
+        }
+      },
+      "email_string": "Jane Doe <jane@example.name>, John Smith <john@example.name>"
+    }
+    d.run(default_tag: 'test') do
+      d.feed(sample_records)
+    end
+    expected = [{f1: "*******@*******.***",
+                 list1: ["*****@*******.***", "*****@*******.***"],
+                 a: {nested:
+                       {field: "*****@*******.******"}},
+                 email_string: "****@*******.****, ****@*******.****"}]
+    assert_equal expected, d.filtered.map{|e| e.last}
+  end
+
+  test "filter_domain_only" do
+    d = create_driver(CONF + %[mode domain_only])
+    sample_records = {
+      "f1": "myEmail@example.net",
+      "list1": [
+        "user1@example.com",
+        "user2@example.org"
+      ],
+      "a": {
+        "nested": {
+          "field": "name3@example.museum"
+        }
+      },
+      "email_string": "Jane Doe <jane@example.name>, John Smith <john@example.name>"
+    }
+    d.run(default_tag: 'test') do
+      d.feed(sample_records)
+    end
+    expected = [{f1: "myEmail@*******.***",
+                 list1: ["user1@*******.***", "user2@*******.***"],
+                 a: {nested:
+                       {field: "name3@*******.******"}},
+                 email_string: "jane@*******.****, john@*******.****"}]
+    assert_equal expected, d.filtered.map{|e| e.last}
+  end
+
+  test "suffix whitelist" do
+    d = create_driver(CONF + %[suffix_whitelist [".example.com", "@example.com"]])
+    sample_records = {
+      "f1": "myEmail@example.net",
+      "list1": [
+        "user1@example.com",
+        "user2@subdomain.example.com"
+      ],
+      "a": {
+        "nested": {
+          "field": "name3@example.museum"
+        }
+      },
+      "email_string": "Jane Doe <jane@example.name>, John Smith <john@example.name>"
+    }
+    d.run(default_tag: 'test') do
+      d.feed(sample_records)
+    end
+    expected = [{f1: "myEma**@*******.***",
+                 list1: ["user1@example.com", "user2@subdomain.example.com"],
+                 a: {nested:
+                       {field: "name*@*******.******"}},
+                 email_string: "jane@*******.****, john@*******.****"}]
+    assert_equal expected, d.filtered.map{|e| e.last}
   end
 
   private
 
-  def create_driver(conf)
+  def create_driver(conf = CONF)
     Fluent::Test::Driver::Filter.new(Fluent::Plugin::EmailObfuscateFilter).configure(conf)
   end
 end
